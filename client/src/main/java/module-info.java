c659917eacdebd0797ac6141a650166ab2ec9bfd
Florian Zapf <zapforama@googlemail.com>
2023-05-03 22:54:52 +0200

    Initial commit

---
+++ client/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.kappa.client {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.kappa.client to javafx.fxml;
+    exports org.kappa.client;
+}
