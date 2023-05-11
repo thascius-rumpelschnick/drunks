fea0443cf3342847b24ef84585926222a77a3692
Florian Zapf <zapforama@googlemail.com>
2023-05-11 17:52:47 +0200

    [Drunks-Client] Setting everything up
    - Ignore sonarlint

--- client/src/main/java/org/kappa/client/DrunksApplication.java
+++ client/src/main/java/org/kappa/client/DrunksApplication.java
@@ -12,7 +12,7 @@ public class DrunksApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(DrunksApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Drunks!");
         stage.setScene(scene);
         stage.show();
     }
