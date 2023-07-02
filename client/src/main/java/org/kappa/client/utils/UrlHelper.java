a12bfa2c853c236718645d2d9f545e55c3c423f9
brodmannerik <106536959+brodmannerik@users.noreply.github.com>
2023-07-02 18:41:54 +0200

    slash issue

--- client/src/main/java/org/kappa/client/utils/UrlHelper.java
+++ client/src/main/java/org/kappa/client/utils/UrlHelper.java
@@ -1,22 +1,27 @@
 package org.kappa.client.utils;
 
 import org.kappa.client.DrunksClientApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.net.URL;
 
 
-public enum UrlHelper {
-  ;
-
+public class UrlHelper {
+  private final static Logger LOGGER = LoggerFactory.getLogger(UrlHelper.class);
 
   public static URL getRessourceAsUrl(final String url) {
-    return DrunksClientApplication.class.getResource("images" + File.separator + url);
+    return DrunksClientApplication.class.getResource("images/" + url);
   }
 
 
   public static String getRessourceAsString(final String url) {
+    try {
+      return getRessourceAsUrl(url).toString();
+    } catch (Exception e) {
+      LOGGER.error(url);
+    }
     return getRessourceAsUrl(url).toString();
   }
-
 }
